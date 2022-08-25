33b4caf78d040f148b3a9c65a09ff1440b2a59b5
lucciolla200 <teoteo.odisharia@gmail.com>
2022-08-25 17:50:11 +0400

    v2

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -8,7 +8,6 @@ import java.util.TreeMap;
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> map = new HashMap<>();
-
         for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
             int key = entry.getKey();
             String value = entry.getValue();
